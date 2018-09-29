f4353b942dea45a5126d3601034a7158a6201d17
hufei <hufeicom@qq.com>
2018-09-29 20:32:06 +0800

    webpack update

--- config/dev.config.js
+++ config/dev.config.js
@@ -2,11 +2,44 @@
  * 开发环境 打包配置文件
  */
 
- module.exports = {
-     devtool: 'source-map'
-     ,devServer: {
-         contentBase: path.resolve(__dirname , './dist')
-         ,port: 8080
-     }
-     ,mode: 'development'
- }
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+const SERVER_BASE = path.resolve(__dirname, '../dist');
+const VIEWS_BASE  = path.resolve(__dirname, '.././src/views/');
+module.exports = {
+    output: {
+        filename: 'static/javascript/[name].js',
+    },
+    plugins: [
+
+        new ExtractTextPlugin('static/css/[name].css'),
+
+        new HtmlWebpackPlugin({
+            title: 'Home',
+            filename: 'home.html',
+            chunks: ['home'],
+            template: 'html-withimg-loader!' + VIEWS_BASE + '/home/home.html'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Blog',
+            filename: 'blog.html',
+            chunks: ['blog'],
+            template: 'html-withimg-loader!' + VIEWS_BASE + '/blog/blog.html'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'About me',
+            filename: 'about.html',
+            chunks: ['about'],
+            template: 'html-withimg-loader!' + VIEWS_BASE + '/about/about.html'
+        })
+    ],
+    devtool: 'source-map'
+    , devServer: {
+        contentBase: SERVER_BASE
+        , port: 8080
+    }
+    , mode: 'development'
+}
--- config/pro.config.js
+++ config/pro.config.js
@@ -1,3 +1,42 @@
 /**
  * 生产环境 打包配置文件
  */
+
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const SERVER_BASE = path.resolve(__dirname, '../dist');
+const VIEWS_BASE  = path.resolve(__dirname, '.././src/views/');
+
+module.exports = {
+    output: {
+        filename: 'static/javascript/[name]_[hash:8].js',
+        publicPath: 'http://127.0.0.1:3000/'
+    },
+    plugins: [
+
+        new ExtractTextPlugin('static/css/[name].css'),
+
+        new HtmlWebpackPlugin({
+            title: 'Home',
+            filename: 'views/home.html',
+            chunks: ['home'],
+            template: 'html-withimg-loader!' + VIEWS_BASE + '/home/home.html'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Blog',
+            filename: 'views/blog.html',
+            chunks: ['blog'],
+            template: 'html-withimg-loader!' + VIEWS_BASE + '/blog/blog.html'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'About me',
+            filename: 'views/about.html',
+            chunks: ['about'],
+            template: 'html-withimg-loader!' + VIEWS_BASE + '/about/about.html'
+        })
+    ],
+    mode: 'production'
+}
