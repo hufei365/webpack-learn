26487abf9a79f414f49d4b649cd887e4c9a9d620
hufei <hufeicom@qq.com>
2018-10-06 12:21:19 +0800

    git submodule

--- config/pro.config.js
+++ config/pro.config.js
@@ -7,40 +7,40 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const SERVER_BASE = path.resolve(__dirname, '../dist');
+const SERVER_BASE = path.resolve(__dirname, '../../public');
 const VIEWS_BASE  = path.resolve(__dirname, '.././src/views/');
 
 module.exports = {
     output: {
-        path: path.resolve(__dirname, '../web-server'),
-        filename: 'public/javascript/[name]_[hash:8].js',
+        path: SERVER_BASE,
+        filename: 'javascript/[name]_[hash:8].js',
         publicPath: 'http://127.0.0.1:3000/'
     },
     plugins: [
 
-        new ExtractTextPlugin('public/css/[name].css'),
+        new ExtractTextPlugin('css/[name]_[hash:8].css'),
 
         new HtmlWebpackPlugin({
             title: 'Home',
-            filename: 'views/home.html',
+            filename: '../views/home.html',
             chunks: ['home'],
             template: 'html-withimg-loader!' + VIEWS_BASE + '/home/home.html'
         }),
         new HtmlWebpackPlugin({
             title: 'Blog',
-            filename: 'views/blog.html',
+            filename: '../views/blog.html',
             chunks: ['blog'],
             template: 'html-withimg-loader!' + VIEWS_BASE + '/blog/blog.html'
         }),
         new HtmlWebpackPlugin({
             title: 'BlogDetail',
-            filename: 'views/blog-detail.html',
+            filename: '../views/blog-detail.html',
             chunks: ['blog'],
             template: 'html-withimg-loader!' + VIEWS_BASE + '/blog/blog-detail.html'
         }),
         new HtmlWebpackPlugin({
             title: 'About me',
-            filename: 'views/about.html',
+            filename: '../views/about.html',
             chunks: ['about'],
             template: 'html-withimg-loader!' + VIEWS_BASE + '/about/about.html'
         })
